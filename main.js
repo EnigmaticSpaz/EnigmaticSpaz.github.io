3fb4518cea17567e042f3ce46c717a06f3673a3a
Liss <Meehanal@oregonstate.edu>
2019-10-14 13:56:41 -0700

    Step 1 Ex. 6

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("mainHeader").onclick = function() {
+    document.getElementById("mainHeader").onmouseover = function() {
       this.style.color = 'orange'
     }
   })
